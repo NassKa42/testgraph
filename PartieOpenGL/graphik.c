872948947afeab04896f180804aabf250a6face1
nas <nassim.kandili@gmail.com>
2025-03-24 16:32:25 +0100

    input is good

--- PartieOpenGL/graphik.c
+++ PartieOpenGL/graphik.c
@@ -4,10 +4,22 @@
 
 int inputIndex = 0;
 char input[100] = ""; 
-int validation = 0;
+int input2Index = 0;
+char input2[100] = "Borne 2"; 
+int input1Index = 0;
+char input1[100] = "Borne 1"; 
 int avancement = 0;
-int borne1;
-int borne2;
+
+
+float xformule = -0.9;
+float yformule = 0.7;
+float xborne1 = -0.9;
+float yborne1 = -0.7;
+float xborne2 = 0.6;
+float yborne2 = -0.7;
+float xtitre;
+float ytitre;
+
 
 void myKey(int c) {
    switch (avancement)
@@ -31,8 +43,10 @@ void myKey(int c) {
             
             break;
         case 13://entrée
-            validation = 1;
             avancement++;
+            break;
+            case 27:
+            avancement--;
             break;
         default:
             if (inputIndex < 99) {
@@ -43,12 +57,65 @@ void myKey(int c) {
             break;
     }
     break;
-    
-   case 2:
-   //bornes
+    case 2://borne 1
+    switch (c) {
+        case 27:
+        avancement--;
+        break;
+        case 8: 
+        case 46: // Touche suppr
+            if(input1Index>0){
+            input1[--input1Index] = '\0';}
+
+            
+            break;
+        case 13://entrée
+            avancement++;
+            break;
+        default:
+            if (input1Index < 99) {
+                input1[input1Index++] = c; // Ajouter le caractère à l'entrée
+                input1[input1Index] = '\0'; // Terminer la chaîne
+                // printf("%s \n", input11);
+            }
+            break;
+    }break;
+
+   case 3: //borne 2
+   switch (c) {
+    case 27:
+        avancement--;
+        break;
+    case 8: 
+    case 46: // Touche suppr
+        if(input2Index>0){
+        input2[--input2Index] = '\0';}
+
+        
+        break;
+    case 13://entrée
+        avancement++;
+        break;
+    default:
+        if (input2Index < 99) {
+            input2[input2Index++] = c; // Ajouter le caractère à l'entrée
+            input2[input2Index] = '\0'; // Terminer la chaîne
+            // printf("%s \n", input21);
+        }
+        break;
+}
    break;
    default:
     break;
+    case 4:
+    switch (c)
+    {
+        case 27:
+        avancement--;
+        break;
+    default:
+        break;
+    }
    }
     
     
@@ -56,34 +123,43 @@ void myKey(int c) {
     
     
 }
-// -1----------> x
+// -1;1  ----------> x  1;1
 // |    
 // |               
 // |      0
 // |
 // |
-// v y          1
+// v y          
+// -1;-1                1;-1
 
 void myDraw(void) {
     setcolor(0.0F, 0.0F, 0.0F);
     char avance[10];
     sprintf(avance, "%d", avancement);    
-    outtextxy(0,0,avance);
+    outtextxy(0.9,-0.8,avance);
+    outtextxy(-0.9, 0.9, "enter pour avancer, esc pour revenir");
     if (avancement==0)
     {
-        outtextxy(-0.9F, 0.9F, "entrer pour formule");
 
     }
     else if (avancement==1)
     {
-        outtextxy(-0.9F, 0.9F, input);
+        outtextxy(xformule,yformule, input);
 
     }
-    else if (avancement == 2)
+    else if (avancement == 2 ) // borne 1
     {
-        outtextxy(-0.9F, 0.9F, input);
-        outtextxy(-0.9, -0.7, "borne 1");
-        outtextxy(0.6,-0.7, "borne 2" );
+        outtextxy(xformule,yformule, input);
+        outtextxy(xborne1, yborne1, input1);
+        outtextxy(xborne2, yborne2, input2);
+    }else if (avancement == 3 ) // borne 2
+    {
+        outtextxy(xformule,yformule, input);
+        outtextxy(xborne1, yborne1, input1);
+        outtextxy(xborne2, yborne2, input2);
+        
+        
+
     }
     
     
